d2730c7ca791c2aba00ffdc00180ae61bf4649b0
Luis Anthony Correa <lucorrea@csumb.edu>
2020-01-28 05:41:19 -0800

    OAuthNeeded New Branch
    Start new branch for OAuth Testing
    Command Functions split into seperate file

--- chatbot_Main.js
+++ chatbot_Main.js
@@ -6,23 +6,27 @@ const tmi = require('tmi.js');
  */
 // Define configuration options
 const opts = {
+    options:{
+        debug: true
+    },
   identity: {
     username: "GauntletBot10",
-    password: "bxxr5c613l3mkl476orulnkq0gabck"
+    password: "oauth:bxxr5c613l3mkl476orulnkq0gabck"
   },
   channels: [
-    "OnePocketPimp"
+    "onepocketpimp"
   ]
 };
 // Create a client with our options
 const client = new tmi.client(opts);
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
 // Connect to Twitch:
 client.connect();
+
+// Register our event handlers (defined below)
+client.on('connected', onConnectedHandler);
+client.on('message', onMessageHandler);
+
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
@@ -34,14 +38,28 @@ function onMessageHandler (target, context, msg, self) {
     
     // If the command is known, let's execute it
     if (commandName === '!dice') {
-        const num = rollDice();
+        try{
+          let commandFile = require(`./Features/chatbot_Function.js`)
+          commandFile.rollDice(client, target, self);
+        } catch (err){
+          console.log(`${err}`);
+          return;
+        }
+        /*const num = rollDice();
         client.say(target, `You rolled a ${num}`);
-        console.log(`* Executed ${commandName} command`);
+        console.log(`* Executed ${commandName} command`);*/
     }
-    else if(commandName === '!play') {
-        const song = play();
-        client.say(target, song);
-        console.log(`* testing ${commandName} command`);
+    else if(commandName === '!playsong' || commandName === '!play') {
+      try{
+        let commandFile = require(`./Features/chatbot_Function.js`)
+        commandFile.playSong(client, target, self);
+      } catch (err){
+        console.log(`${err}`);
+        return;
+      }
+        //const song = play();
+        //client.say(target, song);
+        //console.log(`* testing ${commandName} command`);
     }
     else if(commandName === '!clear'){
         client.say(target, "/clear");
@@ -56,14 +74,19 @@ function onMessageHandler (target, context, msg, self) {
   }
 }
 // Function called when the "dice" command is issued
-function rollDice () {
+/*function rollDice () {
     const sides = 6;
     return Math.floor(Math.random() * sides) + 1;
   }
 function play (){
     return `current song is`;
 }
+*/
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+
+}
+function oAuthAuthorize () {
+    //GET 'https://id.twitch.tv/oauth2/authorize?client_id=<your client ID>&redirect_uri=<your registered redirect URI>&response_type=token&scope=<space-separated list of scopes>'
 }
